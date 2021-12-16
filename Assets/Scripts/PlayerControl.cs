130096c482580ef172802dbf11f908c02ca20985
ashley-lu <43043803+ashley-lu@users.noreply.github.com>
2021-12-15 20:42:02 -0800

    player control

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private int breadHeld { get; set; }
+    private int butterHeld { get; set; }
+    private List<int> jamHeld { get; set; }
+    [SerializeField] private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+        inputVec.y = 0;
+        rb.velocity = inputVec * speed;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         /* when player presses input button */
-            /* set rigidbody velocity to some dir */
+        /* set rigidbody velocity to some dir */
     }
 }
