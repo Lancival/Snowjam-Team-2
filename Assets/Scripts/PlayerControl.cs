695d040c42920a5bc7d4e2cb38e60c1fc02c78b0
Richard Cheng <richard.hokan.cheng@gmail.com>
2021-12-16 00:19:37 -0800

    Fixed duck movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
+
     public int breadHeld { get; set; }
     public int butterHeld { get; set; }
     public List<int> jamHeld { get; set; }
     private bool grounded;
+    
     [SerializeField] private int jumpHeight;
     [SerializeField] private float speed;
 
-    private Animator animator;
+    private Vector2 inputVec = Vector2.zero;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,23 +28,12 @@ public class PlayerControl : MonoBehaviour
         breadHeld = 0;
         butterHeld = 0;
         animator = GetComponent<Animator>();
-        animator.Play("Idle_Anim");
+        //animator.Play("Idle_Anim");
     }
 
     public void OnMove(InputValue input)
     {
-
-        Vector2 inputVec = input.Get<Vector2>();
-        //jump if grounded, don't jump if not
-        inputVec.y = 0;
-        rb.velocity = inputVec * speed;
-
-    }
-
-    public void OnJump(InputValue input)
-    {
-        Debug.LogError("spacebar!");
-        rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        inputVec = input.Get<Vector2>();
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -56,10 +52,19 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        /* when player presses input button */
-        /* set rigidbody velocity to some dir */
+        // Set x-velocity without changing y-velocity
+        rb.velocity = new Vector2(inputVec.x * speed, rb.velocity.y);
+
+        // Flip duck direction
+        if (inputVec.x > 0)
+            transform.localScale = Vector3.one;
+        else if (inputVec.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Jump only if grounded
+        if (inputVec.y > 0 && grounded)
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
     }
 }
