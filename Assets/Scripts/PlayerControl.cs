5a806a1e9ed7184dbe3a1fef7c17130ad6825617
Austin Law <austinlaw8@gmail.com>
2021-12-15 19:39:23 -0800

    Updated project

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /* when player presses input button */
+            /* set rigidbody velocity to some dir */
+    }
+}
